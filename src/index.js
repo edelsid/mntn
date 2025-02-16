b1d382710d49c106f72012e03a3db0667e2c8b3c
edelsid <eradonna@yandex.ru>
2025-02-16 23:16:02 +0300

    added intersection observer for anchor change

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 const scss = require("./style.scss");
 
 const anchors = Array.from(document.querySelectorAll(".slider__item"));
-const anchorSet = document.querySelector(".slider");
 const scrollButton = document.querySelector(".scrollup");
+const observables = Array.from(document.querySelectorAll(".stages__item"));
+const banner = document.querySelector(".header");
+observables.push(banner);
 
-//Ссылки-якоря
-anchorSet.addEventListener("click", (e) => {
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5,
+}
+
+//Определение активного якоря
+const callback = (entries) => {
+  let target;
+  entries.forEach((entry) => {
+    const classes = entry.target.classList;
+    if (entry.isIntersecting && !classes.contains("header")) {
+      target = anchors.find((item) => 
+        item.attributes.name.value === entry.target.id);
+    } else if (entry.isIntersecting && classes.contains("header")) {
+      target = anchors[0]
+    }
+    if (target) changeAnchor(target);
+  })
+}
+
+//Смена цвета границы активного якоря
+const changeAnchor = (target) => {
   anchors.forEach((anchor) => {
     anchor.classList.remove("active");
   });
-  e.target.parentElement.classList.add("active");
-});
+  target.classList.add("active");
+}
 
 //Скролл для смартфонов
 document.addEventListener("scroll", () => {
@@ -27,3 +50,8 @@ scrollButton.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
+//Наблюдение за тем, на какой части сайта мы находимся
+const observer = new IntersectionObserver(callback, options);
+observables.forEach((item) => {
+  observer.observe(item);
+});
